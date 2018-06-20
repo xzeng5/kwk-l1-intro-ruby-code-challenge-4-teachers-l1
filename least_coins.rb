01956ca6e58610deeb0b9a70d0c5eec13d5f1c5b
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-20 22:26:35 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -14,9 +14,3 @@ def least_coins(cents)
   }
 end
 
-# I want to write a program in which
-# cents will continue to divide through
-# 25, 10, 5, and finally 1 until the remainder becomes zero
-# To do so, I would probably need an array of 25, 10, 5, 1
-# Then, I'll use .each method to cycle through each element of the array
-# At the same time, the output will be sorted into a hash
